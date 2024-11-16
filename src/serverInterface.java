fe55edeb2712b86848ae11a65b64f9c6af8c188d
alexyan06 <yan517@purdue.edu>
2024-11-16 18:22:52 -0500

    Create serverInterface.java

---
+++ src/serverInterface.java
@@ -0,0 +1,22 @@
+/**
+ * CS 180 Group Project: serverInterface
+ *
+ * Defines serverInterface, which is the interface of Server.
+ */
+
+public interface serverInterface {
+
+    /**
+     * Fields:
+     * private static final String SERVER_IP = "localhost"; Defines the Server IP as local
+     * for the use of the program.
+     * private static final int SERVER_PORT = 12346; Defines the port number for the server
+     * for use in the program.
+     *
+     * Constructors:
+     * none
+     *
+     * Methods:
+     * Server only contains a Main Method
+     */
+}
