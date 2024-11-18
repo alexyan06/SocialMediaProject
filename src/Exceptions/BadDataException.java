70202a27207e6c54e4a544a481fdc09c90951231
srimodugula <smodugul@purdue.edu>
2024-11-17 23:34:59 -0500

    Update BadDataException.java

--- src/Exceptions/BadDataException.java
+++ src/Exceptions/BadDataException.java
@@ -1,3 +1,12 @@
+/**
+ * CS 180 Group Project: Server
+ * @author Alex Yan, yan517
+ * @author Sripoorna Modugula, smodugul
+ * @version 1.0 11/17/2024
+ * Server that connects to multiple clients and
+ * does the processing of data.
+ */
+
 public class BadDataException extends Exception {
     public BadDataException(String message) {
         super(message);
