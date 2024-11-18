3ecddb2ac7227083583218dc3ab1e395614848c9
srimodugula <smodugul@purdue.edu>
2024-11-17 23:36:07 -0500

    Update InvalidTargetException.java

--- src/Exceptions/InvalidTargetException.java
+++ src/Exceptions/InvalidTargetException.java
@@ -1,5 +1,14 @@
-public class InvalidTargetException extends Exception {
-    public InvalidTargetException(String message) {
+/**
+ * CS 180 Group Project: Server
+ * @author Alex Yan, yan517
+ * @author Sripoorna Modugula, smodugul
+ * @version 1.0 11/17/2024
+ * Server that connects to multiple clients and
+ * does the processing of data.
+ */
+
+public class BadDataException extends Exception {
+    public BadDataException(String message) {
         super(message);
     }
 }
